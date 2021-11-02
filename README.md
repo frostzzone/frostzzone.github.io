743ec8065fe922d66f2da628b4303cf422321217
frostzzone <65735427+frostzzone@users.noreply.github.com>
2021-11-02 20:02:52 +0000

    Add WIP

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# frostzzone.github.io
-Website im making
+# THIS IS A WIP
+## [frostzzone.github.io](https://frostzzone.github.io)
+My Homepage
+
+### Also on [repl](https://frostzzone.repl.co/)
---
+++ theme.js
@@ -0,0 +1,27 @@
+window.onload = function() {
+  
+      // Try to read from local storage, otherwise set to default
+      let currentTheme = localStorage.getItem("mytheme") || "Default";
+
+      setTheme("Default", currentTheme);
+
+      const themeSelector = document.getElementById("theme-selector");
+      themeSelector.value = currentTheme;
+
+      themeSelector.addEventListener("change", function(e) {
+        const newTheme = e.currentTarget.value;
+        setTheme(currentTheme, newTheme);
+      });
+
+      function setTheme(oldTheme, newTheme) {
+        const body = document.getElementsByTagName("body")[0];
+
+        body.classList.remove(oldTheme);
+        body.classList.add(newTheme);
+
+        currentTheme = newTheme;
+
+        // Store the new theme in local storage
+        localStorage.setItem("mytheme", newTheme);
+      }
+    };
